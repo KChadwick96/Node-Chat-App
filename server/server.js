c6add0c851f49738497b3f4f204f997877e2c6de
Kieran Chadwick <kieran.chadwick@outlook.com>
2018-03-02 17:48:04 +0000

    base socket.io setup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => console.log("Server is up..."));
+server.listen(port, () => console.log("Server is up..."));
