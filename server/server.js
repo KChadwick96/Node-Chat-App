8c8b7fe064e68920c0f2570863870e232dd32391
Kieran Chadwick <kieran.chadwick@outlook.com>
2018-03-06 15:26:40 +0000

    moved js to own file + setup basic message listeners

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Bob',
+        text: 'the message text',
+        created_at: new Date()
+    })
+
+    socket.on('createMessage', function(message) {
+        console.log('createMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('Client disconected');
     });
